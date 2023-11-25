f5a4abd36ead156b0b042077987dc31d8c868e8d
MrunalMB <107421478+MrunalMB@users.noreply.github.com>
2023-11-25 19:02:24 +0530

    GFG-Palindromic Array.java

---
+++ GFG-Palindromic Array.java
@@ -0,0 +1,78 @@
+Given a Integer array A[] of n elements. Your task is to complete the function PalinArray. Which will return 1 if all the elements of the Array are palindrome otherwise it will return 0.
+
+Example 1:
+
+Input:
+5
+111 222 333 444 555
+
+Output:
+1
+
+Explanation:
+A[0] = 111 //which is a palindrome number.
+A[1] = 222 //which is a palindrome number.
+A[2] = 333 //which is a palindrome number.
+A[3] = 444 //which is a palindrome number.
+A[4] = 555 //which is a palindrome number.
+As all numbers are palindrome so This will return 1.
+Example 2:
+
+Input:
+3
+121 131 20
+ 
+Output:
+0
+
+Explanation:
+20 is not a palindrome hence the output is 0.
+
+Constraints:
+1 <=T<= 50
+1 <=n<= 20
+1 <=A[]<= 10000
+  //{ Driver Code Starts
+import java.util.*;
+class PalindromicArray{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+			int n = sc.nextInt();
+			int[] a = new int[n];
+			for(int i = 0 ;i < n; i++)
+				a[i]=sc.nextInt();
+			GfG g = new GfG();
+			System.out.println(g.palinArray(a , n));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the Function below*/
+class GfG
+{
+	public static int palinArray(int[] a, int n)
+           {
+                  int flage=1;
+                  for(int i=0;i<n;i++){
+                      int sum=0;
+                      int num=a[i];
+                      int org=num;
+                      while(num>0)
+                      {
+                        int   r=num%10;
+                          sum= sum*10+r;
+                          num=num/10;
+                      }
+                     // System.out.println(sum);
+                      if(sum!=org){
+                          flage=0;
+                      }
+                  }
+                  return flage;
+           }
+}
