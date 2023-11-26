1148ea82a31949f09cf02d77b40cb79c571d5846
MrunalMB <107421478+MrunalMB@users.noreply.github.com>
2023-11-26 22:59:58 +0530

    GFG-Smaller and Larger.java

---
+++ GFG-Smaller and Larger.java
@@ -0,0 +1,85 @@
+Given a sorted array Arr of size N and a value X, find the number of array elements less than or equal to X and elements more than or equal to X. 
+
+Example 1:
+
+Input:
+N = 7, X = 0
+Arr[] = {1, 2, 8, 10, 11, 12, 19}
+Output: 0 7
+Explanation: There are no elements less or
+equal to 0 and 7 elements greater or equal
+to 0.
+Example 2:
+
+Input:
+N = 7, X = 5
+Arr[] = {1, 2, 8, 10, 11, 12, 19}
+Output: 2 5
+Explanation: There are 2 elements less or
+equal to 5 and 5 elements greater or equal
+to 5.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function getMoreAndLess() which takes the array of integers arr, n and x as parameters and returns an array of size 2. pair denoting the answer. The first value is number of elements less than or equal to x and the second value is number of elements more than or equal to x.
+
+Expected Time Complexity: O(logN)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 105
+0 ≤ X ≤ 106
+1 ≤ Arr[i] ≤ 106
+  //{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans[] = new Solve().getMoreAndLess(arr, n, x);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solve {
+    int[] getMoreAndLess(int[] arr, int n, int x) {
+        int c=0,c1=0;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]<=x){
+                c++;
+            }if(arr[i]>=x){
+                c1++;
+            }
+        }
+        return new int[]{c,c1};
+        // code here
+    }
+}
