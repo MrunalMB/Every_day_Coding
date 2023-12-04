85990736f92a6a96ab34ec4c68ed4de5058c9c7e
MrunalMB <107421478+MrunalMB@users.noreply.github.com>
2023-12-04 22:11:10 +0530

    GFG-Cyclically rotate an array by one.java

---
+++ GFG-Cyclically rotate an array by one.java
@@ -0,0 +1,85 @@
+Given an array, rotate the array by one position in clock-wise direction.
+ 
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1, 2, 3, 4, 5}
+Output:
+5 1 2 3 4
+ 
+
+Example 2:
+
+Input:
+N = 8
+A[] = {9, 8, 7, 6, 4, 2, 1, 3}
+Output:
+3 9 8 7 6 4 2 1
+ 
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function rotate() which takes the array A[] and its size N as inputs and modify the array in place.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1<=N<=105
+0<=a[i]<=105
+  //{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            obj.rotate(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+//User function Template for Java
+
+class Compute {
+    
+    public void rotate(int arr[], int n)
+    {
+     int ans[] =new int[n];
+     ans[0]=arr[n-1];
+     int c=1;
+     for(int i=0;i<n-1;i++){
+         ans[c]=arr[i];
+         c++;
+     }
+     for(int i=0;i<n;i++){
+         arr[i]=ans[i];
+     }
+    }
+}
