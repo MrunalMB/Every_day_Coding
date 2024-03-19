95340d5a01c254e584e9bf7acb8eba2500a452d0
MrunalMB <107421478+MrunalMB@users.noreply.github.com>
2024-03-19 23:28:02 +0530

    Print 1 To N Without Loop.java

---
+++ Print 1 To N Without Loop.java
@@ -0,0 +1,18 @@
+
+
+class Solution
+{
+    
+  public void printNos(int n)
+    {
+        //Your code here
+        if(n<=0){
+            return;
+        }
+        printNos(n-1);
+        System.out.print(n+" ");
+    }
+    
+    
+}
+
